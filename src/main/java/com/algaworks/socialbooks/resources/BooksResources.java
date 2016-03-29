e2c0c1ce493a17e85347f2298316f7cc437737de
Igor Vieira <higorc2@gmail.com>
2016-03-29 16:25:05 -0300

    Use POST to create one booke resource

--- src/main/java/com/algaworks/socialbooks/resources/BooksResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/BooksResources.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resources;
 import com.algaworks.socialbooks.domain.Book;
 import com.algaworks.socialbooks.repository.BooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("/books")
 public class BooksResources {
 
     @Autowired
     private BooksRepository booksRepository;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public List<Book> list() {
         return booksRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void save(@RequestBody Book book) {
+        booksRepository.save(book);
     }
 }
