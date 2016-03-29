6296fd6f6704ab95645d8f4997f785bbb89572dd
Igor Vieira <higorc2@gmail.com>
2016-03-29 13:58:57 -0300

    Create representation for the book resource

---
+++ src/main/java/com/algaworks/socialbooks/domain/Book.java
@@ -0,0 +1,83 @@
+package com.algaworks.socialbooks.domain;
+
+import java.util.Date;
+import java.util.List;
+
+public class Book {
+
+    private Long id;
+
+    private String title;
+
+    private Date datePublished;
+
+    private String publisher;
+
+    private String summary;
+
+    private List<Review> reviews;
+
+    private String author;
+
+    public Book() {}
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDatePublished() {
+        return datePublished;
+    }
+
+    public void setDatePublished(Date datePublished) {
+        this.datePublished = datePublished;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
--- src/main/java/com/algaworks/socialbooks/resources/BooksResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/BooksResources.java
@@ -1,14 +1,22 @@
 package com.algaworks.socialbooks.resources;
 
+import com.algaworks.socialbooks.domain.Book;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class BooksResources {
 
     @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public String list() {
-        return "Rest Aplicado, Git passo-a-passo";
+    public List<Book> list() {
+
+        Book b1 = new Book("Git passo-a-passo");
+        Book b2 = new Book("Rest Aplicado");
+        Book[] books = {b1, b2};
+        return Arrays.asList(books);
     }
 }
